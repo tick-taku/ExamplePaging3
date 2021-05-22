c93665a3ba5092f6fd089a510b159722eb0d1931
tick-taku <tick.taku.77@gmail.com>
2021-05-23 01:53:40 +0900

    Add ImageViewBindingAdapter

---
+++ app/src/main/java/com/tick/taku/example/paging3/core/ImageViewBindingAdapter.kt
@@ -0,0 +1,15 @@
+package com.tick.taku.example.paging3.core
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+import coil.request.CachePolicy
+
+@BindingAdapter("app:load", "app:cachePolicy", requireAll = false)
+fun ImageView.loadImage(url: String?, diskCachePolicy: CachePolicy = CachePolicy.DISABLED) {
+    url?.takeIf { it.isNotEmpty() }?.let {
+        load(it) {
+            diskCachePolicy(diskCachePolicy)
+        }
+    }
+}
