78467bf9f81ba8ae668cf62cd06a6d9e67666b45
tick-taku <tick.taku.77@gmail.com>
2021-05-23 01:56:44 +0900

    Add search breeds api

--- app/src/main/java/com/tick/taku/example/paging3/data/api/CatSearchApi.kt
+++ app/src/main/java/com/tick/taku/example/paging3/data/api/CatSearchApi.kt
@@ -1,5 +1,6 @@
 package com.tick.taku.example.paging3.data.api
 
+import com.tick.taku.example.paging3.entity.Breed
 import com.tick.taku.example.paging3.entity.Cat
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -10,5 +11,9 @@ interface CatSearchApi {
     @Headers("x-api-key: apiKey")
     @GET("images/search")
     suspend fun cats(@Query("limit") limit: Int, @Query("page") page: Int): List<Cat>
+
+    @Headers("x-api-key: apiKey")
+    @GET("breeds")
+    suspend fun breeds(@Query("limit") limit: Int, @Query("page") page: Int): List<Breed>
 
 }
