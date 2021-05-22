5521f04bf96adde701456c2e01e9796a15415b1b
tick-taku <tick.taku.77@gmail.com>
2021-05-23 01:00:36 +0900

    Implement repository

---
+++ app/src/main/java/com/tick/taku/example/paging3/data/repository/internal/CatRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.tick.taku.example.paging3.data.repository.internal
+
+import com.tick.taku.example.paging3.data.api.CatSearchApi
+import com.tick.taku.example.paging3.data.repository.CatRepository
+import com.tick.taku.example.paging3.entity.Cat
+import com.tick.taku.example.paging3.gateway.ApiClient
+
+internal class CatRepositoryImpl(
+    private val apiClient: ApiClient
+): CatRepository {
+
+    override suspend fun cat(page: Int): List<Cat> =
+        apiClient.provideRetrofit().create(CatSearchApi::class.java)
+            .cats(20, page)
+
+}
