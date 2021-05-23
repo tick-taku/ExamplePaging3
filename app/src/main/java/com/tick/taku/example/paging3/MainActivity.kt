7974deb1e067b7552c2397855746ac7c54002715
tick-taku <tick.taku.77@gmail.com>
2021-05-23 17:11:57 +0900

    Show cats

--- app/src/main/java/com/tick/taku/example/paging3/MainActivity.kt
+++ app/src/main/java/com/tick/taku/example/paging3/MainActivity.kt
@@ -2,10 +2,40 @@ package com.tick.taku.example.paging3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.PagingData
+import com.tick.taku.example.paging3.core.provideViewModels
+import com.tick.taku.example.paging3.data.repository.internal.CatRepositoryImpl
+import com.tick.taku.example.paging3.databinding.ActivityMainBinding
+import com.tick.taku.example.paging3.entity.Cat
+import com.tick.taku.example.paging3.gateway.ApiClient
+import com.tick.taku.example.paging3.ui.viewmodel.CatViewModel
+import com.tick.taku.example.paging3.ui.widget.CatPagingAdapter
+import com.tick.taku.example.paging3.usecase.internal.CatUseCaseImpl
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    private val viewModel: CatViewModel by provideViewModels {
+        CatViewModel(CatUseCaseImpl(CatRepositoryImpl(ApiClient())))
+    }
+
+    private val listAdapter = CatPagingAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.catList.adapter = listAdapter
+
+        viewModel.cats.observe(this) {
+            listAdapter.submitWhenStarted(it)
+        }
     }
+
+    private fun CatPagingAdapter.submitWhenStarted(data: PagingData<Cat>) =
+        lifecycleScope.launchWhenStarted { submitData(data) }
 }
