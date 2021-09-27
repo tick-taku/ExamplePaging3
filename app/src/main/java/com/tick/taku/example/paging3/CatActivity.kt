bc1b3c7cf263165311594710bb3623a801805dc5
tick-taku <tick.taku.77@gmail.com>
2021-09-27 14:06:32 +0900

    Rename Activity

--- app/src/main/java/com/tick/taku/example/paging3/CatActivity.kt
+++ app/src/main/java/com/tick/taku/example/paging3/CatActivity.kt
@@ -5,10 +5,9 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
-import androidx.paging.LoadState
 import androidx.paging.PagingData
 import com.tick.taku.example.paging3.core.andRefreshable
-import com.tick.taku.example.paging3.databinding.ActivityMainBinding
+import com.tick.taku.example.paging3.databinding.ActivityCatBinding
 import com.tick.taku.example.paging3.entity.Cat
 import com.tick.taku.example.paging3.ui.viewmodel.CatViewModel
 import com.tick.taku.example.paging3.ui.widget.CatPagingAdapter
@@ -16,10 +15,10 @@ import com.tick.taku.example.paging3.ui.widget.CatPagingLoadStateAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class CatActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    private val binding: ActivityCatBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_cat)
     }
 
     private val viewModel: CatViewModel by viewModels()
