42718b09a5ff0cd71b1634a2c0208cfda9322b14
tick-taku <tick.taku.77@gmail.com>
2021-09-27 15:00:42 +0900

    Use IO thread

--- app/src/main/java/com/tick/taku/example/paging3/data/repository/internal/CatRepositoryImpl.kt
+++ app/src/main/java/com/tick/taku/example/paging3/data/repository/internal/CatRepositoryImpl.kt
@@ -3,15 +3,18 @@ package com.tick.taku.example.paging3.data.repository.internal
 import com.tick.taku.example.paging3.data.api.CatSearchApi
 import com.tick.taku.example.paging3.data.repository.CatRepository
 import com.tick.taku.example.paging3.entity.Cat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CatRepositoryImpl @Inject internal constructor(
     private val catSearchApi: CatSearchApi
 ): CatRepository {
 
-    override suspend fun cats(limit: Int, page: Int): Result<List<Cat>> =
-        runCatching {
+    override suspend fun cats(limit: Int, page: Int): Result<List<Cat>> = runCatching {
+        withContext(Dispatchers.IO) {
             catSearchApi.cats(limit, page)
         }
+    }
 
 }
